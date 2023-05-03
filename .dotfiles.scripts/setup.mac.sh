dee8e450919ae5bb782f04337840b37649e8ae5b
Janez Justin <janez.justin0@gmail.com>
2023-05-04 00:00:52 +0200

    feat: update setup scripts

---
+++ .dotfiles.scripts/setup.mac.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function wait_for_press() {
+    read -n 1 -p "$1. Press a key to continue."
+}
+
+xcode-select --install
+
+wait_for_press "install xcode tools"
+
+# Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+brew install fzf
+# To install useful key bindings and fuzzy completion:
+$(brew --prefix)/opt/fzf/install
+
+# Brew installation
+brew install $(<brew.txt)
+brew install --cask $(<brew-cask.txt)
+
+echo "SET 'MesloLGS NF' as your terminal's font"
