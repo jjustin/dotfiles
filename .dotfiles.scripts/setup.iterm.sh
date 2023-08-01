dbaaef95c9fd50677261a3309a227c2a5e1718d5
Janez Justin <janez.justin0@gmail.com>
2023-08-01 20:21:43 +0200

    feat: add iterm setup script

---
+++ .dotfiles.scripts/setup.iterm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/.iterm"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
