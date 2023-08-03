c1d687cde6d07ae07994e9bcfb89fcf26937c623
Janez Justin <janez.justin0@gmail.com>
2023-08-03 23:30:51 +0200

    feat: update brew packages to install only missing ones

--- .dotfiles.scripts/install.brew-packages.sh
+++ .dotfiles.scripts/install.brew-packages.sh
@@ -4,5 +4,17 @@ if ! command -v brew &> /dev/null; then
     exit 1
 fi
 
-brew install $(<brew.txt)
-brew install --cask $(<brew-cask.txt)
+formulas=$(diff --new-line-format="" --unchanged-line-format="" <(sort brew.txt) <(sort <(brew list --formula -1)))
+if [ -z "$formulas" ]; then
+	echo "No formulas to install"
+else
+	brew install --formula $formulas
+fi
+
+casks=$(diff --new-line-format="" --unchanged-line-format="" <(sort brew-cask.txt) <(sort <(brew list --cask -1)))
+if [ -z "$casks" ]; then
+	echo "No casks to install"
+else
+	brew install --cask $casks
+fi
+
