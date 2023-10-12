c08ff7e5e488cda96938095683ef487423f9a417
Janez Justin <janez.justin0@gmail.com>
2023-10-12 17:55:47 +0200

    feat: add defaults script

---
+++ .dotfiles.scripts/setup.defaults.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# CMD + Option + T open tab to the right
+defaults write com.brave.Browser NSUserKeyEquivalents -dict \
+    "New Tab to the Right" "@~t"
--- .dotfiles.scripts/setup.sh
+++ .dotfiles.scripts/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 $(basename "$0")/setup.iterm.sh
+$(basename "$0")/setup.defaults.sh
