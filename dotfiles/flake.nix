8d7187b6af80e1cf2d2658bf5ade2e2f4e3b80cc
Janez Justin <janez.justin0@gmail.com>
2024-03-07 19:36:04 +0100

    feat: use unstable channel

--- dotfiles/flake.nix
+++ dotfiles/flake.nix
@@ -2,7 +2,7 @@
   description = "jjustin's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
