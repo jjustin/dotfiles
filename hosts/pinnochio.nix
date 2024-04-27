41276d8bc53ce88aa42b7b5220ffaab7237f2663
Janez Justin <janez.justin0@gmail.com>
2024-04-27 23:56:36 +0200

    fix: Update moved variables

--- hosts/pinnochio.nix
+++ hosts/pinnochio.nix
@@ -42,13 +42,13 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Enable CUPS to print documents.
