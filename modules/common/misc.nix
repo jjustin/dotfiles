29b2067400e07ce065f8da1061d7daa87bf4d7e3
Janez Justin <janez.justin0@gmail.com>
2024-06-28 19:50:15 +0200

    fix: Compile warnings

--- modules/common/misc.nix
+++ modules/common/misc.nix
@@ -5,7 +5,7 @@
 
   # Check: is this darwin only?
   fonts.fontDir.enable = true;
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     meslo-lgs-nf
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
--- modules/home-manager/programs/zsh/default.nix
+++ modules/home-manager/programs/zsh/default.nix
@@ -8,7 +8,7 @@
   programs.zsh = {
     enable = true;
 
-    enableAutosuggestions = true;
+    autosuggestion.enable = true;
     syntaxHighlighting = {
       enable = true;
     };
