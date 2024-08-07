ebfd869f1f64994988942e9c357ca859a35f7b8e
Janez Justin <janez.justin0@gmail.com>
2024-08-07 16:09:13 +0200

    update flake dependencies

--- modules/common/misc.nix
+++ modules/common/misc.nix
@@ -3,8 +3,6 @@
 {
   environment.variables.EDITOR = "nvim";
 
-  # Check: is this darwin only?
-  fonts.fontDir.enable = true;
   fonts.packages = with pkgs; [
     meslo-lgs-nf
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
