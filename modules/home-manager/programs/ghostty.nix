aa8492a2a9aa3bf7bf81b54bed02bf43e58391fb
Janez Justin <janez.justin0@gmail.com>
2025-01-08 09:44:58 +0100

    ghostty config

---
+++ modules/home-manager/programs/ghostty.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.my.ghostty;
+in
+{
+  options.my = with lib; {
+    ghostty = {
+      config = mkOption {
+        type = types.attrsOf types.anything;
+        default = { };
+        description = "Extra ghostty configuration.";
+      };
+    };
+  };
+
+  config = {
+    xdg.configFile."ghostty/config" =
+      {
+
+        text = lib.generators.toKeyValue
+          {
+            mkKeyValue = lib.generators.mkKeyValueDefault { } " = ";
+            listsAsDuplicateKeys = true;
+          }
+          cfg.config;
+      };
+
+    my.ghostty.config = {
+      confirm-close-surface = false;
+      keybind = [
+        "global:ctrl+super+/=toggle_quick_terminal"
+      ];
+    };
+  };
+}
