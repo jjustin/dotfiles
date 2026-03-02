df02423573468b097c970a15ad93e9ca2ca8772a
Janez Justin <janez.justin0@gmail.com>
2026-03-02 20:52:00 +0100

    configure midi synth

--- hosts/gaming.nix
+++ hosts/gaming.nix
@@ -78,7 +78,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -127,5 +127,9 @@
 
     usbutils # lsusb
     wineWowPackages.stable
+    
+    helvum     # graphical patchbay for PipeWire - MIDI + audio inspection
+    fluidsynth # Synth engine
+    qsynth     # GUI for fluidsynth
   ];
 }
